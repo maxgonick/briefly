acbdacfc58dada5d7fde0a4ec7c70f7ef72a6242
Theo Zhang <azhang8@g.ucla.edu>
2023-04-22 10:53:01 -0700

    Created BillButton card that links to another page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 //import EmailCallTabs from '@/components/EmailCallTabs'
 import Blurb from "../components/Blurb"
+import { Box } from '@mui/material'
+import BillButton from '@/components/BillButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +14,9 @@ export default function Home() {
     <main>
     <Header/>
       <Blurb/>
+      <Box>
+        <BillButton billID={'bill name'}/>
+      </Box>
       {/* <EmailCallTabs email="dear joe, hi. -donnie j trump " number="4206661738"/> */}
     <Footer/>
     </main>
