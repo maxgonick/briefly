117ad92505ff09cff95bf71ae4678353c9c52842
smmzhu <smmzhu@gmail.com>
2023-04-22 09:45:11 -0700

    created emailcalltabs component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-import Blurb from "./components/Blurb"
+import Blurb from "./components/Blurb";
+import EmailCallTabs from "./components/EmailCallTabs";
 
 export default function Home() {
   return (
-    <Blurb/>
+    <main>
+      <Blurb/>
+      <EmailCallTabs email="hi sleepy joe." number="1234567890"/>
+    </main>
   );
 }
