e7e9f55e079bca41fadda7ea12bc694941b419ca
smmzhu <smmzhu@gmail.com>
2023-04-21 23:33:57 -0700

    added header (footer is wrong rn)

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {TextField} from '@mui/material';
+const SearchBar = () => {
+    return(
+        <TextField variant="outlined" sx={{ input: { backgroundColor: 'white' } }} />
+    );
+};
+
+export default SearchBar;
