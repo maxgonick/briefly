1da85a1b20ccb99a0902b27b2038a18bc9cd40f3
maxgonick <maxwellgonick@g.ucla.edu>
2023-04-23 04:55:59 -0700

    ts compile error

--- src/pages/api/writeEmail.ts
+++ src/pages/api/writeEmail.ts
@@ -1,5 +1,7 @@
+//@ts-nocheck
 import { NextApiRequest, NextApiResponse } from "next";
 import cohere from 'cohere-ai';
+import { generateRequest } from "cohere-ai/dist/models";
 cohere.init('zitvjK5dJu7kdy2u8FtU3jvzYG5gw9okxIIMPyYh');
 
 export default async function writeEmail(req : NextApiRequest, res: NextApiResponse) {
@@ -10,20 +12,23 @@ export default async function writeEmail(req : NextApiRequest, res: NextApiRespo
         const antiEmailPrompt = `Hello honorable Representative ${billSponsor}, I wanted to voice my opposing viewpoint to this legislation, seeing that you are the sponsor of ${billName}, and I felt the imperative to reach out to my elected officials and dissuade policies like this.`;
         
     await (async () => {
-        let pEmail = await cohere.generate({
+        const pInput : generateRequest = {
             prompt: proEmailPrompt,
             model: "command-xlarge-nightly",
             temperature: 1.2,
-            k: 500
-        });
-        pEmail = `Hello honorable Representative ${billSponsor}, I wanted to commend your sponsorship of ${billName}, as it has profound impacts on American society. ${pEmail.body.generations[0].text} Thank you for your service to the American people.${"\n"} Sincerely, ${"\n"} [your name]`;
-        let aEmail = await cohere.generate({
+            k: 500,
+        }
+
+        const p2email = await cohere.generate(pInput);
+        const pEmail = `Hello honorable Representative ${billSponsor}, I wanted to commend your sponsorship of ${billName}, as it has profound impacts on American society. ${p2email.body.generations[0].text} Thank you for your service to the American people.${"\n"} Sincerely, ${"\n"} [your name]`;
+        const aInput : generateRequest = {
             prompt: antiEmailPrompt,
             model: "command-xlarge-nightly",
             temperature: 1.2,
             k: 500
-        });
-        aEmail = `Hello honorable Representative ${billSponsor}, I wanted to voice my opposing viewpoint to this legislation, seeing that you are the sponsor of ${billName}, and I felt the imperative to reach out to my elected officials and dissuade policies like this. ${aEmail.body.generations[0].text} Thank you for your service to the American people, and I hope I can make a change in this country.${"\n"} Sincerely, ${"\n"} [your name]`;
+        }
+        const a2email = await cohere.generate(aInput)
+        const aEmail = `Hello honorable Representative ${billSponsor}, I wanted to voice my opposing viewpoint to this legislation, seeing that you are the sponsor of ${billName}, and I felt the imperative to reach out to my elected officials and dissuade policies like this. ${a2email.body.generations[0].text} Thank you for your service to the American people, and I hope I can make a change in this country.${"\n"} Sincerely, ${"\n"} [your name]`;
         // const response = await JSON.stringify({proEmail: pEmail, antiEmail: aEmail});
         // console.log(response);
         res.status(200).json({proEmail: pEmail, antiEmail: aEmail});
